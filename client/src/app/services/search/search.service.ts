433fa2fc10fcc9e843826f4c3349ac8d44c140c1
Brian Park <park-brian@users.noreply.github.com>
2021-10-14 01:43:31 -0400

    add method stubs

--- client/src/app/services/search/search.service.ts
+++ client/src/app/services/search/search.service.ts
@@ -5,4 +5,8 @@ import { Injectable } from "@angular/core";
 })
 export class SearchService {
   constructor() {}
+
+  getCodeFromDescription(description: string) {}
+
+  getDescriptionFromCode(code: string) {}
 }
