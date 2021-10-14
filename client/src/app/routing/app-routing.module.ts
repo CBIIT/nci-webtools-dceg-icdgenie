0026ef0bd809254e87397a400aa565897709c77d
Brian Park <park-brian@users.noreply.github.com>
2021-10-14 01:36:18 -0400

    add routing

---
+++ client/src/app/routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes, RouteReuseStrategy } from "@angular/router";
+import { HomeComponent } from "../home/home.component";
+import { SearchComponent } from "../search/search.component";
+import { AboutComponent } from "../about/about.component";
+import { CustomReuseStrategy } from "./custom-reuse-strategy";
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "search", component: SearchComponent },
+  { path: "about", component: AboutComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "**", redirectTo: "" },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  providers: [{ provide: RouteReuseStrategy, useClass: CustomReuseStrategy }],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
