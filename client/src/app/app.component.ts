49a1f9fe4e3128cfe4b9e8f3465d8785a74cadda
Brian Park <park-brian@users.noreply.github.com>
2021-10-14 01:38:34 -0400

    create root component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-root",
@@ -6,5 +7,11 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "client";
+  links = [
+    { title: "Home", target: "/home" },
+    { title: "Search", target: "/search" },
+    { title: "About", target: "/about" },
+  ];
+
+  constructor(public route: ActivatedRoute) {}
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
-import { AppRoutingModule } from "./app-routing.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AppRoutingModule } from "./routing/app-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { SearchComponent } from "./search/search.component";
+import { AboutComponent } from "./about/about.component";
+import { SearchService } from "./services/search/search.service";
 import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FileValueAccessorDirective } from "./directives/file-value-accessor.directive";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, SearchComponent, AboutComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  providers: [SearchService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
