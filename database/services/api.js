84df86fad9e127736fbc43eb662c5b2631d88458
Brian Park <park-brian@users.noreply.github.com>
2021-11-24 20:26:10 -0500

    use database service

--- database/services/api.js
+++ database/services/api.js
@@ -1,21 +1,20 @@
 const { Router, json } = require("express");
-const sqlite = require("better-sqlite3");
 const icdgenie = require("./icdgenie");
-const config = require("../config.json");
 
 const api = Router();
-const database = sqlite(config.database);
 
 api.use(json());
 
 api.get("/ping", (request, response) => {
+  const { logger, database } = request.app.locals;
+  logger.debug("ping");
   const results = icdgenie.getStatus(database);
   response.json(results);
 });
 
 api.get("/search", (request, response) => {
-  const { logger } = request.app.locals;
-  logger.debug("Search: " + JSON.stringify(request.query));
+  const { logger, database } = request.app.locals;
+  logger.debug("search: " + JSON.stringify(request.query));
   const results = icdgenie.search(database, request.query);
   response.json(results);
 });
