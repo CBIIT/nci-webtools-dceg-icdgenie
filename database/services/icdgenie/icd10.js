64a04ec8294d8a028bf2ea33b387f2a2459cef65
Brian Park <park-brian@users.noreply.github.com>
2021-12-07 00:19:23 -0500

    include parent records

--- database/services/icdgenie/icd10.js
+++ database/services/icdgenie/icd10.js
@@ -1,9 +1,27 @@
 function searchInjuryTable(database, { code, description }) {
   if (code) {
-    return database.prepare(`SELECT * FROM icd10_injury WHERE code LIKE :code order by code`).all({ code: `${code}%` });
+    return database
+      .prepare(
+        `with parents as (
+          select * from icd10_injury where
+            code like :code
+          union all
+          select i.* from icd10_injury i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, code`,
+      )
+      .all({ code: `${code}%` });
   } else if (description) {
     return database
-      .prepare(`SELECT * FROM icd10_injury WHERE path like :description order by path`)
+      .prepare(
+        `with parents as (
+          select * from icd10_injury where
+            path like :description
+          union all
+          select i.* from icd10_injury i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, path`,
+      )
       .all({ description: `%${description}%` });
   } else {
     return database.prepare(`SELECT * FROM icd10_injury`).all();
@@ -14,18 +32,31 @@ function searchDrugTable(database, { code, description }) {
   if (code) {
     return database
       .prepare(
-        `SELECT * FROM icd10_drug WHERE 
-        poisoningAccidental LIKE :code OR
-        poisoningIntentionalSelfHarm LIKE :code OR
-        poisoningAssault LIKE :code OR
-        poisoningUndetermined LIKE :code OR
-        adverseEffect LIKE :code OR
-        underdosing LIKE :code`,
+        `with parents as (
+          select * from icd10_drug where
+            poisoningAccidental like :code or
+            poisoningIntentionalSelfHarm like :code or
+            poisoningAssault like :code or
+            poisoningUndetermined like :code or
+            adverseEffect like :code or
+            underdosing like :code
+          union all
+          select i.* from icd10_drug i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, path`,
       )
       .all({ code: `${code}%` });
   } else if (description) {
     return database
-      .prepare(`SELECT * FROM icd10_drug WHERE path like :description order by path`)
+      .prepare(
+        `with parents as (
+          select * from icd10_drug where
+            path like :description
+          union all
+          select i.* from icd10_drug i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, path`,
+      )
       .all({ description: `%${description}%` });
   } else {
     return database.prepare(`SELECT * FROM icd10_drug`).all();
@@ -36,18 +67,31 @@ function searchNeoplasmTable(database, { code, description }) {
   if (code) {
     return database
       .prepare(
-        `SELECT * FROM icd10_neoplasm WHERE 
-        malignantPrimary LIKE :code OR
-        malignantSecondary LIKE :code OR
-        carcinomaInSitu LIKE :code OR
-        benign LIKE :code OR
-        uncertainBehavior LIKE :code OR
-        unspecifiedBehavior LIKE :code`,
+        `with parents as (
+          select * from icd10_neoplasm where
+            malignantPrimary like :code or
+            malignantSecondary like :code or
+            carcinomaInSitu like :code or
+            benign like :code or
+            uncertainBehavior like :code or
+            unspecifiedBehavior like :code
+          union all
+          select i.* from icd10_neoplasm i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, path`,
       )
       .all({ code: `${code}%` });
   } else if (description) {
     return database
-      .prepare(`SELECT * FROM icd10_neoplasm WHERE path like :description order by path`)
+      .prepare(
+        `with parents as (
+          select * from icd10_neoplasm where
+            path like :description 
+          union all
+          select i.* from icd10_neoplasm i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, path`,
+      )
       .all({ description: `%${description}%` });
   } else {
     return database.prepare(`SELECT * FROM icd10_neoplasm`).all();
@@ -56,10 +100,28 @@ function searchNeoplasmTable(database, { code, description }) {
 
 function searchIndexTable(database, { code, description }) {
   if (code) {
-    return database.prepare(`SELECT * FROM icd10 WHERE code like :code order by code`).all({ code: `${code}%` });
+    return database
+      .prepare(
+        `with parents as (
+          select * from icd10 where
+            code like :code
+          union all
+          select i.* from icd10 i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, code`,
+      )
+      .all({ code: `${code}%` });
   } else if (description) {
     return database
-      .prepare(`SELECT * FROM icd10 WHERE path like :description order by path`)
+      .prepare(
+        `with parents as (
+          select * from icd10 where
+            path like :description
+          union all
+          select i.* from icd10 i
+            join parents p on i.id = p.parentId
+        ) select distinct * from parents order by level, path`,
+      )
       .all({ description: `%${description}%` });
   } else {
     return database.prepare(`SELECT * FROM icd10`).all();
