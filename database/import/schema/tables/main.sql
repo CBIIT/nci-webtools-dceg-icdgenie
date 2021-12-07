b5248997c48bfca86d8943fc60599c53dcb9c7bb
Brian Park <park-brian@users.noreply.github.com>
2021-12-06 22:02:24 -0500

    add icd10 tables to schema

--- database/import/schema/tables/main.sql
+++ database/import/schema/tables/main.sql
@@ -1,14 +1,53 @@
 create table "icd10"
 (
     "id" integer primary key,
+    "parentId" integer,
+    "path" text,
+    "code" text,
+    "description" text,
+    "level" integer,
+    "parentCode" text
+);
+
+create table "icd10_drug"
+(
+    "id" integer primary key,
+    "parentId" integer,
+    "path" text,
+    "substance" text,
+    "level" integer,
+    "parentSubstance" text,
+    "poisoningAccidental" text,
+    "poisoningIntentionalSelfHarm" text,
+    "poisoningAssault" text,
+    "poisoningUndetermined" text,
+    "adverseEffect" text,
+    "underdosing" text
+);
+
+create table "icd10_injury"
+(
+    "id" integer primary key,
+    "parentId" integer,
+    "path" text,
+    "description" text,
+    "level" integer,
+    "parentDescription" text,
+    "code" text
+);
+
+create table "icd10_neoplasm"
+(
+    "id" integer primary key,
+    "parentId" integer,
+    "path" text,
     "neoplasm" text,
     "level" integer,
-    "parent" text,
+    "parentNeoplasm" text,
     "malignantPrimary" text,
     "malignantSecondary" text,
     "carcinomaInSitu" text,
     "benign" text,
     "uncertainBehavior" text,
-    "unspecifiedBehavior" text,
-    "other" text
+    "unspecifiedBehavior" text
 );
