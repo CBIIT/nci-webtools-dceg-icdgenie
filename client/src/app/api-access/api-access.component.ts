c297737544b224bf5d4d0d18892346114cf08f1c
Brian Park <park-brian@users.noreply.github.com>
2021-12-15 20:38:39 -0500

    add api-access component

---
+++ client/src/app/api-access/api-access.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-api-access",
+  templateUrl: "./api-access.component.html",
+  styleUrls: ["./api-access.component.scss"],
+})
+export class ApiAccessComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
