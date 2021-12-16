5c1482491d7ada2c6a7a59bdf4d7e6a63d7b76c6
Brian Park <park-brian@users.noreply.github.com>
2021-12-15 20:39:26 -0500

    add api-access route

--- client/src/app/routing/app-routing.module.ts
+++ client/src/app/routing/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes, RouteReuseStrategy } from "@angular/router";
 import { HomeComponent } from "../home/home.component";
 import { SearchComponent } from "../search/search.component";
 import { AboutComponent } from "../about/about.component";
+import { ApiAccessComponent } from "../api-access/api-access.component";
 import { CustomReuseStrategy } from "./custom-reuse-strategy";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "search", component: SearchComponent },
   { path: "about", component: AboutComponent },
+  { path: "api-access", component: ApiAccessComponent },
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "**", redirectTo: "" },
 ];
