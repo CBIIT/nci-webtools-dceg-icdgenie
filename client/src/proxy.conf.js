4f365d1cfc8bde8cb0bbbe010640d06303a669ba
Brian Park <park-brian@users.noreply.github.com>
2021-12-15 20:38:22 -0500

    load configuration using dotenv

--- client/src/proxy.conf.js
+++ client/src/proxy.conf.js
@@ -1,8 +1,10 @@
-const config = require("../../database/config.json");
+require("dotenv").config({
+  path: require("path").resolve(__dirname, "../../database/.env"),
+});
 
 module.exports = {
   "/api": {
-    target: `http://localhost:${config?.port || 9000}`,
+    target: `http://localhost:${process.env.API_PORT || 9000}`,
     secure: false,
     logLevel: "debug",
   },
