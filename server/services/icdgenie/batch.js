40cab606725e58a402d55f684fb1de098816f29c
Brian Park <park-brian@users.noreply.github.com>
2022-03-16 20:35:41 -0400

    improve performance of batch queries

--- server/services/icdgenie/batch.js
+++ server/services/icdgenie/batch.js
@@ -6,87 +6,78 @@ function batchExport(database, query) {
   const { inputType, input, outputType } = query;
   const inputs = input
     .split(/\n/g)
-    .map((input) => input.trim())
+    .map((input) => input.trim().replace(/\"/g, ""))
     .filter((input) => input.length > 0);
   let results = [];
+
+  database.exec(`
+    drop table if exists input;
+    create temporary table input (input text);
+  `);
+
+  for (let input of inputs) {
+    database.prepare(`insert into input values (?)`).run([input]);
+  }
 
   if (inputType === "icdo3") {
     // batch export all children of icdo3 codes
     if (outputType === "icdo3") {
-      for (const code of inputs) {
-        results = results.concat(
-          icdo3.search(database, { code }).map((result) => ({
-            input: code,
-            icdo3: result.code,
-            icdo3Description: result.description,
-          })),
-        );
-      }
+      results = database
+        .prepare(
+          `select input, code as icdo3, description as icdo3Description
+        from input join icdo3_morphology on icdo3_morphology.code like input.input || '%'`,
+        )
+        .all();
     }
 
     // batch translate all icdo3 codes to icd10 codes
     else if (outputType === "icd10") {
-      for (const code of inputs) {
-        results = results.concat(
-          translate.translateCode(database, { icdo3: code }).map((result) => ({
-            input: code,
-            icd10: result.icd10,
-            icd10Description: result.icd10Description,
-          })),
-        );
-      }
+      results = database
+        .prepare(
+          `select input, icd10, icd10Description
+        from input join icd10_icdo3_mapping on icd10_icdo3_mapping.icdo3 like input.input || '%'`,
+        )
+        .all();
     }
   } else if (inputType === "icd10") {
     // batch translate all icd10 codes to icdo3 codes
     if (outputType === "icdo3") {
-      for (const code of inputs) {
-        results = results.concat(
-          translate.translateCode(database, { icd10: code }).map((result) => ({
-            input: code,
-            icdo3: result.icdo3,
-            icdo3Description: result.icdo3Description,
-          })),
-        );
-      }
+      results = database
+        .prepare(
+          `select input, icdo3, icdo3Description
+        from input join icd10_icdo3_mapping on icd10_icdo3_mapping.icd10 like input.input || '%'`,
+        )
+        .all();
     }
 
     // batch export all children of icd10 codes
     else if (outputType === "icd10") {
-      for (const code of inputs) {
-        results = results.concat(
-          icd10.search(database, { code }).map((result) => ({
-            input: code,
-            icd10: result.code,
-            icd10Description: result.description,
-          })),
-        );
-      }
+      results = database
+        .prepare(
+          `select input, code as icd10, description as icd10Description
+        from input join icd10 on icd10.code like input.input || '%'`,
+        )
+        .all();
     }
   } else {
     // batch export all icdo3 codes/descriptions containing the query
     if (outputType === "icdo3") {
-      for (const query of inputs) {
-        results = results.concat(
-          icdo3.search(database, { query }).map((result) => ({
-            input: query,
-            icdo3: result.code,
-            icdo3Description: result.description,
-          })),
-        );
-      }
+      results = database
+        .prepare(
+          `select input, code as icdo3, description as icdo3Description 
+        from input join icdo3_morphology on icdo3_morphology.description like '%' || input.input || '%'`,
+        )
+        .all();
     }
 
     // batch export all icd10 codes/descriptions containing the query
     else if (outputType === "icd10") {
-      for (const query of inputs) {
-        results = results.concat(
-          icd10.search(database, { query }).map((result) => ({
-            input: query,
-            icd10: result.code,
-            icd10Description: result.description,
-          })),
-        );
-      }
+      results = database
+        .prepare(
+          `select input, code as icd10, description as icd10Description 
+        from input join icd10 on icd10.path like '%' || input.input || '%'`,
+        )
+        .all();
     }
   }
 
