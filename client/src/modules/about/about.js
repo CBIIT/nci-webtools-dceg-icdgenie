eb80dc166f82cd70fd4e169050995ccfe45c2fc5
Brian Park <park-brian@users.noreply.github.com>
2022-03-31 14:56:16 -0400

    update about page header

--- client/src/modules/about/about.js
+++ client/src/modules/about/about.js
@@ -2,11 +2,14 @@ import Container from "react-bootstrap/Container";
 
 export default function About() {
   return (
-    <div className="h-100" style={{backgroundColor: 'white'}}>
-      <Container className="py-4 shadow-sm h-100">
-        <h2 className="text-primary">About</h2>
-        <hr />
+    <div className="h-100 bg-white">
+      <Container className="py-5">
+        <h1 className="display-6 text-muted text-center text-uppercase ">About</h1>
+      </Container>
 
+      <hr />
+
+      <Container className="py-5">
         <p>Placeholder for About Page</p>
       </Container>
     </div>
