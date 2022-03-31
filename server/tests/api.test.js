ed8b2d5ba650615f738517e54fd0f8712f54973e
Brian Park <park-brian@users.noreply.github.com>
2022-03-31 17:06:11 -0400

    update tests

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -1,14 +1,13 @@
-const path = require("path");
 const express = require("express");
 const request = require("supertest");
-const sqlite = require("better-sqlite3");
-const { logs, database } = require("../config");
 const { api } = require("../services/api");
 const { getLogger } = require("../services/logger");
+const { loadDatabaseInMemory } = require("../services/database");
+const { APP_NAME, DATABASE_PATH, LOG_LEVEL } = process.env;
 
 const app = express();
-app.locals.logger = getLogger("test", logs);
-app.locals.database = sqlite(database);
+app.locals.logger = getLogger(APP_NAME, { level: LOG_LEVEL });
+app.locals.database = loadDatabaseInMemory(DATABASE_PATH);
 app.use("/api", api);
 
 test("api is defined", () => {
@@ -21,24 +20,23 @@ test("GET api/ping returns true", async () => {
 });
 
 test("GET api/search returns icd10 results when searching by code", async () => {
-  const params = { code: "C76.2" };
-  const response = await request(app).get("/api/search").query(params);
-  const responseValues = response.body.icd10.map((record) => Object.values(record)).flat();
+  const params = { code: "C76.2", format: "list" };
+  const response = await request(app).get("/api/search/icd10").query(params);
+  const responseValues = response.body.map((record) => Object.values(record)).flat();
   expect(responseValues).toContain(params.code);
 });
 
 test("GET api/search returns icd10 results when searching by description", async () => {
-  const params = { description: "neoplasm" };
-  const response = await request(app).get("/api/search").query(params);
+  const params = { description: "neoplasm", format: "list" };
+  const response = await request(app).get("/api/search/icd10").query(params);
 
   // icd10 results should not be empty
-  const icd10Results = response.body.icd10;
+  const icd10Results = response.body;
   expect(icd10Results.length).toBeGreaterThan(0);
 
-  // all icd10 results should contain the search term
+  // icd10 results should contain the search term
   const descriptionRegex = new RegExp(params.description, "i");
-  for (const result of icd10Results) {
-    const resultContainsDescription = descriptionRegex.test(result.neoplasm) || descriptionRegex.test(result.parent);
-    expect(resultContainsDescription).toBe(true);
-  }
+  const responseValues = response.body.map((record) => Object.values(record)).flat();
+  const hasDescription = responseValues.some((value) => descriptionRegex.test(value));
+  expect(hasDescription).toBe(true);
 });
