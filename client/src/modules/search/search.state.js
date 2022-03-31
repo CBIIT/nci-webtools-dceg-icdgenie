4f37ae0d9c0fec1bca930b5923d2083c127c9d21
Brian Park <park-brian@users.noreply.github.com>
2022-03-31 14:58:17 -0400

    implement selectors

--- client/src/modules/search/search.state.js
+++ client/src/modules/search/search.state.js
@@ -1,17 +1,33 @@
-import { atom } from "recoil";
+import { atom, selectorFamily } from "recoil";
+import { getSearchResults } from "./search.utils";
 
-export const defaultFormState = {
-  search: "",
-  indexData: [],
-  neoplasmData: [],
-  drugData: [],
-  injuryData: [],
-  icdo3Data: [],
-  loading: false,
-  submitted: false,
-};
+export const searchState = atom({
+  key: "search.searchState",
+  default: "",
+});
 
-export const formState = atom({
-  key: "search.formState",
-  default: defaultFormState,
+export const resultsSelector = selectorFamily({
+  key: "search.resultsSelector",
+  get:
+    (query) =>
+    async ({ get }) => {
+      return query?.length > 0
+        ? await getSearchResults(query)
+        : {
+            indexData: [],
+            neoplasmData: [],
+            drugData: [],
+            injuryData: [],
+            icdo3Data: [],
+          };
+    },
+});
+
+export const modalState = atom({
+  key: "search.modalState",
+  default: {
+    open: false,
+    title: "",
+    body: "",
+  },
 });
