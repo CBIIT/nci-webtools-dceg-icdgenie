4b2ef15ea05acb52b59849659c63003bbf8d121f
Brian Park <park-brian@users.noreply.github.com>
2022-03-31 14:56:27 -0400

    update api-access header

--- client/src/modules/api-access/api-access.js
+++ client/src/modules/api-access/api-access.js
@@ -3,13 +3,16 @@ import SwaggerUI from "swagger-ui-react";
 
 export default function ApiAccess() {
   return (
-    <div className="h-100" style={{backgroundColor: 'white'}}>
-      <Container className="py-4 shadow-sm h-100">
-        <h2 className="text-primary">API Access</h2>
-        <hr />
+    <div className="h-100 bg-white">
+      <Container className="py-5">
+        <h1 className="display-6 text-muted text-center text-uppercase ">API Access</h1>
+      </Container>
 
+      <hr />
+
+      <Container className="py-5">
         <p>
-          The ICDGenie API provides programmatic access to endpoints which allow users to search and translate ICD-10
+          The ICD Genie API provides programmatic access to endpoints which allow users to search and translate ICD-10
           and ICD-O-3 codes. The following resources are available:
         </p>
         <SwaggerUI url={process.env.PUBLIC_URL + "/api"} />
