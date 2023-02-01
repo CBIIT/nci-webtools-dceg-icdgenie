6c039e23b25eaa7f444e6c67098af3ab614a5b26
Brian Park <park-brian@users.noreply.github.com>
2023-01-31 23:34:10 -0500

    add protocol

--- database/import.sh
+++ database/import.sh
@@ -5,9 +5,9 @@ set -x
 # node opensearch.js
 
 # add/update documents
-curl -k -XPOST -u "$ADMIN:$PASSWORD" "$DOMAIN/_bulk" --data-binary @data/icd10drug.json -H "Content-Type: application/json"
-curl -k -XPOST -u "$ADMIN:$PASSWORD" "$DOMAIN/_bulk" --data-binary @data/icd10eindex.json -H "Content-Type: application/json"
-curl -k -XPOST -u "$ADMIN:$PASSWORD" "$DOMAIN/_bulk" --data-binary @data/icd10neoplasm.json -H "Content-Type: application/json"
-curl -k -XPOST -u "$ADMIN:$PASSWORD" "$DOMAIN/_bulk" --data-binary @data/icd10tabular.json -H "Content-Type: application/json"
-curl -k -XPOST -u "$ADMIN:$PASSWORD" "$DOMAIN/_bulk" --data-binary @data/icdo3.json -H "Content-Type: application/json"
-curl -k -XPOST -u "$ADMIN:$PASSWORD" "$DOMAIN/_bulk" --data-binary @data/translations.json -H "Content-Type: application/json"
+curl -k -XPOST -u "$ADMIN:$PASSWORD" "https://$DOMAIN/_bulk" --data-binary @data/icd10drug.json -H "Content-Type: application/json"
+curl -k -XPOST -u "$ADMIN:$PASSWORD" "https://$DOMAIN/_bulk" --data-binary @data/icd10eindex.json -H "Content-Type: application/json"
+curl -k -XPOST -u "$ADMIN:$PASSWORD" "https://$DOMAIN/_bulk" --data-binary @data/icd10neoplasm.json -H "Content-Type: application/json"
+curl -k -XPOST -u "$ADMIN:$PASSWORD" "https://$DOMAIN/_bulk" --data-binary @data/icd10tabular.json -H "Content-Type: application/json"
+curl -k -XPOST -u "$ADMIN:$PASSWORD" "https://$DOMAIN/_bulk" --data-binary @data/icdo3.json -H "Content-Type: application/json"
+curl -k -XPOST -u "$ADMIN:$PASSWORD" "https://$DOMAIN/_bulk" --data-binary @data/translations.json -H "Content-Type: application/json"
