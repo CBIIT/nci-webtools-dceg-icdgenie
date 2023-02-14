95fbb666dac5f1af0a34c6cb83692a6d56f037f7
Brian Park <park-brian@users.noreply.github.com>
2023-02-14 00:20:01 -0500

    fix responsive css issues

--- client/src/modules/about/about.js
+++ client/src/modules/about/about.js
@@ -2,7 +2,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import AboutImage from "./images/about.png";
 
 export default function About() {
-
   return (
     <div className="h-100 bg-white">
       <Container className="py-3">
@@ -11,25 +10,39 @@ export default function About() {
 
       <hr />
 
-
-
       <Row>
-        <Col md={4}>
-          <img src={AboutImage} style={{width: "150%", aspectRatio: "1.5"}}></img>
+        <Col lg={4} className="mw-lg-100">
+          <img src={AboutImage} style={{ width: "150%", aspectRatio: "1.5" }}></img>
         </Col>
-        <Col md={8}>
+        <Col lg={8}>
           <Container className="py-5">
             <h5 className="my-4 blue-subheader">A Little Background</h5>
-            <p>Accurate histological classification is important for facilitating studies of cancer epidemiology and etiologic heterogeneity. ICD Genie is a web-based tool that can assist epidemiologists, pathologists, research assistants, and data scientists to more easily access, translate and validate codes and text descriptions from the International Classification of Diseases (10th Edition) and International Classification of Diseases for Oncology, 3rd Edition (ICD-O-3).</p>
+            <p>
+              Accurate histological classification is important for facilitating studies of cancer epidemiology and
+              etiologic heterogeneity. ICD Genie is a web-based tool that can assist epidemiologists, pathologists,
+              research assistants, and data scientists to more easily access, translate and validate codes and text
+              descriptions from the International Classification of Diseases (10th Edition) and International
+              Classification of Diseases for Oncology, 3rd Edition (ICD-O-3).
+            </p>
 
-            <p>Although many of the early classification and coding conventions have remained unchanged in successive versions of ICD and ICD-O, substantial revisions have been made in more recent versions i.e., ICD-10 and ICD-O-3. Depending on when data were collected, epidemiological studies and cancer databases may employ different ICD versions, thereby hampering data harmonization. In addition, tumor-related data may be available only in pathology reports, in text formats, or as ICD codes, which can be difficult to translate for non-specialists.</p>
+            <p>
+              Although many of the early classification and coding conventions have remained unchanged in successive
+              versions of ICD and ICD-O, substantial revisions have been made in more recent versions i.e., ICD-10 and
+              ICD-O-3. Depending on when data were collected, epidemiological studies and cancer databases may employ
+              different ICD versions, thereby hampering data harmonization. In addition, tumor-related data may be
+              available only in pathology reports, in text formats, or as ICD codes, which can be difficult to translate
+              for non-specialists.
+            </p>
 
-            <p>To address these challenges, we developed ICD Genie as a publicly available web tool to facilitate the translation of ICD-10 and ICD-O-3 codes to human-readable text and vice versa. By improving accessibility and making existing cancer classification and coding schemes more readily understandable and searchable, ICD Genie will help accelerate descriptive and molecular epidemiological studies of cancer.</p>
+            <p>
+              To address these challenges, we developed ICD Genie as a publicly available web tool to facilitate the
+              translation of ICD-10 and ICD-O-3 codes to human-readable text and vice versa. By improving accessibility
+              and making existing cancer classification and coding schemes more readily understandable and searchable,
+              ICD Genie will help accelerate descriptive and molecular epidemiological studies of cancer.
+            </p>
           </Container>
         </Col>
       </Row>
-
-
     </div>
   );
 }
--- client/src/modules/about/resources.js
+++ client/src/modules/about/resources.js
@@ -7,29 +7,51 @@ export default function Resources() {
         <h1 className="display-6 page-header text-muted text-center text-uppercase">Resources</h1>
       </Container>
       <hr />
-      <Row>
-        <Col md={5}>
-          <Container className="py-5 ms-5">
-            <h5 className="my-4 blue-subheader">ICD Genie utilizes data from the following publicly available resources:</h5>
+      <Container fluid>
+        <Row>
+          <Col md={5}>
+            <Container className="py-5 ps-3">
+              <h5 className="my-4 blue-subheader">
+                ICD Genie utilizes data from the following publicly available resources:
+              </h5>
 
-            <p>ICD-10 diagnosis codes:</p>
-            <ul>
-              <li><a href="https://www.cms.gov/files/zip/2022-code-tables-tabular-and-index.zip" target="_blank">ICD-10-CM codes maintained by Centers for Medicare & Medicaid Services (CMS)</a></li>
-            </ul>
+              <p>ICD-10 diagnosis codes:</p>
+              <ul>
+                <li>
+                  <a href="https://www.cms.gov/files/zip/2022-code-tables-tabular-and-index.zip" target="_blank">
+                    ICD-10-CM codes maintained by Centers for Medicare & Medicaid Services (CMS)
+                  </a>
+                </li>
+              </ul>
 
-            <p>ICD-O-3 morphology codes and description sources:</p>
-            <ul>
-              <li><a href="https://www.naaccr.org/icdo3/" target="_blank">North American Association of Central Cancer Registries (NAACCR)</a></li>
-              <li><a href="https://seer.cancer.gov/icd-o-3/" target="_blank">Surveillance, Epidemiology, and End Results (SEER) program validation list</a></li>
-              <li><a href="https://apps.who.int/iris/bitstream/handle/10665/96612/9789241548496_eng.pdf" target="_blank">World Health Organization (WHO) ICD-O-3 publication</a></li>
-            </ul>
-          </Container>
-        </Col>
-        <Col md={7}>
-          <img src={ResourcesImage} style={{width: "100%"}}></img>
-        </Col>
-      </Row>
+              <p>ICD-O-3 morphology codes and description sources:</p>
+              <ul>
+                <li>
+                  <a href="https://www.naaccr.org/icdo3/" target="_blank">
+                    North American Association of Central Cancer Registries (NAACCR)
+                  </a>
+                </li>
+                <li>
+                  <a href="https://seer.cancer.gov/icd-o-3/" target="_blank">
+                    Surveillance, Epidemiology, and End Results (SEER) program validation list
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://apps.who.int/iris/bitstream/handle/10665/96612/9789241548496_eng.pdf"
+                    target="_blank"
+                  >
+                    World Health Organization (WHO) ICD-O-3 publication
+                  </a>
+                </li>
+              </ul>
+            </Container>
+          </Col>
+          <Col md={7} className="px-0">
+            <img src={ResourcesImage} className="w-100 img-fluid"></img>
+          </Col>
+        </Row>
+      </Container>
     </div>
-
-  )
+  );
 }
