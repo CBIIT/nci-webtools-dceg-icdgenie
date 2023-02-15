d18818adaae0375c96ba97cb2a77fb27a779335f
bchen49 <benchen4998@gmail.com>
2023-02-15 15:16:02 -0500

    set tab to icdo3 if icd10 has no results

--- client/src/modules/search/search.results.js
+++ client/src/modules/search/search.results.js
@@ -5,20 +5,34 @@ import { resultsSelector } from "./search.state";
 import ICD10 from "./search.icd10";
 import ICDO3 from "./search.icdo3";
 import ICD10Hierarchy from "./search.hierarchy";
+import { useEffect, useState } from "react";
 
 export default function SearchResults({ query, maps }) {
   const results = useRecoilValue(resultsSelector(query));
+  const [tab, setTab] = useState("icd10CodeTable")
+
+  useEffect(() => {
+    if (maps.icdo3.length === 0)
+      setTab("icd10CodeTable")
+    else if (maps.tabular.size === 0 && maps.neoplasm.size === 0 && maps.drug.size === 0 && maps.injury.size === 0)
+      setTab("icdo3CodeTable")
+    else 
+      setTab("icd10CodeTable")
+  }, [maps])
+  
   return (
     <Tabs
       id="results-tabs"
-      defaultActiveKey="icd10CodeTable"
-      className="d-flex justify-content-center bg-primary-light">
+      activeKey={tab}
+      className="d-flex justify-content-center bg-primary-light"
+      onSelect={(e) => setTab(e)}
+      >
       <Tab eventKey="icd10CodeTable" title="ICD-10 Code Table">
-   
-          <ICD10 form={results} maps={maps} />
+
+        <ICD10 form={results} maps={maps} />
       </Tab>
       <Tab eventKey="icdo3CodeTable" title="ICD-O-3 Code Table">
-          <ICDO3 form={results} maps={maps} />
+        <ICDO3 form={results} maps={maps} />
       </Tab>
       {/*<Tab eventKey="contact" title="ICD-10 Hierarchy">
         <div className="bg-light">
