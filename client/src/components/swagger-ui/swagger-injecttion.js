2d0d63e2bd47987a27f7d5c4592988cf8a7bef8d
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2024-11-05 15:16:52 -0500

    Add label to request body for swagger ui

--- client/src/components/swagger-ui/swagger-injecttion.js
+++ client/src/components/swagger-ui/swagger-injecttion.js
@@ -7,6 +7,8 @@ const SwaggerButtonLabelInjector = () => {
       // Select buttons with specific classes
       const copyButtons = document.querySelectorAll('.copy-to-clipboard button');
       const downloadButtons = document.querySelectorAll('.download-contents');
+      const textareas = document.querySelectorAll('.body-param__text');
+
 
       let labelsAdded = 0;
 
@@ -22,6 +24,24 @@ const SwaggerButtonLabelInjector = () => {
       downloadButtons.forEach((button) => {
         if (!button.getAttribute('aria-label')) {
           button.setAttribute('aria-label', 'Download Contents');
+          labelsAdded++;
+        }
+      });
+      // Add label to textarea if it doesn't have one
+      textareas.forEach((textarea, index) => {
+        if (!textarea.previousSibling || !textarea.previousSibling.classList.contains('custom-label')) {
+          // Create a unique id for the textarea
+          const textareaId = `custom-textarea-${index}`;
+          textarea.setAttribute('id', textareaId);
+
+          // Create a label element with a 'for' attribute
+          const label = document.createElement('label');
+          label.textContent = 'Enter your search query:';
+          label.className = 'custom-label';
+          label.setAttribute('for', textareaId);
+
+          // Insert the label before the textarea
+          textarea.parentNode.insertBefore(label, textarea);
           labelsAdded++;
         }
       });
