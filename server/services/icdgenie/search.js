ba8634c882e4be140133e21b11626549c03717bb
huuaho <32457341+huuaho@users.noreply.github.com>
2026-05-19 11:00:03 -0400

    Add ICD-10-PCS to backend search query

--- server/services/icdgenie/search.js
+++ server/services/icdgenie/search.js
@@ -115,12 +115,13 @@ async function opensearch(request, response) {
     "size": 500
   }
 
-  const [tabularResult, neoplasmResult, drugResult, injuryResult, icdo3Result] = await Promise.all([
+  const [tabularResult, neoplasmResult, drugResult, injuryResult, icdo3Result, icd10pcsResult] = await Promise.all([
     client.search({ index: "tabular", body }),
     client.search({ index: "neoplasm", body }),
     client.search({ index: "drug", body }),
     client.search({ index: "injury", body }),
-    client.search({ index: "icdo3", body })
+    client.search({ index: "icdo3", body }),
+    client.search({ index: "icd10pcs", body })
   ])
 
 
@@ -130,6 +131,7 @@ async function opensearch(request, response) {
     drug: drugResult.body.hits.hits,
     injury: injuryResult.body.hits.hits,
     icdo3: icdo3Result.body.hits.hits,
+    icd10pcs: icd10pcsResult.body.hits.hits,
     showSuggestions: true,
     fuzzyTerms: [],
   }
@@ -139,14 +141,16 @@ async function opensearch(request, response) {
   const drugOptions = drugResult.body.suggest["spell-check"][0].options;
   const injuryOptions = injuryResult.body.suggest["spell-check"][0].options;
   const icdo3Options = !search.includes("/") ? icdo3Result.body.suggest["spell-check"][0].options : []
+  const icd10pcsOptions = icd10pcsResult.body.suggest["spell-check"][0].options;
 
-  if (results.tabular.length || results.neoplasm.length || results.drug.length || results.injury.length || results.icdo3.length) {
-    const [tabularFuzzy, neoplasmFuzzy, drugFuzzy, injuryFuzzy, icdo3Fuzzy] = await Promise.all([
+  if (results.tabular.length || results.neoplasm.length || results.drug.length || results.injury.length || results.icdo3.length || results.icd10pcs.length) {
+    const [tabularFuzzy, neoplasmFuzzy, drugFuzzy, injuryFuzzy, icdo3Fuzzy, icd10pcsFuzzy] = await Promise.all([
       fuzzySearch(tabularOptions, client, "tabular"),
       fuzzySearch(neoplasmOptions, client, "neoplasm"),
       fuzzySearch(drugOptions, client, "drug"),
       fuzzySearch(injuryOptions, client, "injury"),
-      fuzzySearch(icdo3Options, client, "icdo3")
+      fuzzySearch(icdo3Options, client, "icdo3"),
+      fuzzySearch(icd10pcsOptions, client, "icd10pcs")
     ])
 
     results.tabular = results.tabular.concat(tabularFuzzy)
@@ -154,6 +158,7 @@ async function opensearch(request, response) {
     results.drug = results.drug.concat(drugFuzzy)
     results.injury = results.injury.concat(injuryFuzzy)
     results.icdo3 = results.icdo3.concat(icdo3Fuzzy)
+    results.icd10pcs = results.icd10pcs.concat(icd10pcsFuzzy)
     results.showSuggestions = false
   }
   else {
@@ -163,7 +168,8 @@ async function opensearch(request, response) {
       ...neoplasmOptions.filter(e => e.score >= minScore).map(e => e.text),
       ...drugOptions.filter(e => e.score >= minScore).map(e => e.text),
       ...injuryOptions.filter(e => e.score >= minScore).map(e => e.text),
-      ...icdo3Options.filter(e => e.score >= minScore).map(e => e.text)
+      ...icdo3Options.filter(e => e.score >= minScore).map(e => e.text),
+      ...icd10pcsOptions.filter(e => e.score >= minScore).map(e => e.text)
     ])]
   }
 
