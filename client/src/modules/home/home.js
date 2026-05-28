2d7ba7a8bf00fef5a9c38ac89bacd4e94b231f9a
huuaho <32457341+huuaho@users.noreply.github.com>
2026-05-28 15:03:09 -0400

    revert home page search placeholder to match prod

--- client/src/modules/home/home.js
+++ client/src/modules/home/home.js
@@ -28,7 +28,7 @@ export default function Home() {
                 search={search}
                 setSearch={setSearch}
                 handleSubmit={handleSubmit}
-                placeholder="Search by Keywords, ICD-10, ICD-O-3, ICD-11 or ICD-O-4 code"
+                placeholder="Search ICDGenie"
               />
             </Col>
           </Row>
